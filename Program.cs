06227bbb4f02d16635c068b04d153894f1dc8d28
miguel-enrique <miguelxzsmaster@gmail.com>
2020-03-30 20:24:59 -0300

    exercicio ola exercicio de ola  :wq!

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual o seu nome?: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá " + nome);
+            Console.ReadKey();
+        }
+    }
+}
